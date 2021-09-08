edbdadaed67da6878c91d1a62066ffc8ff71b0a4
flaviobenyaakov <flaviobenyaakov@gmail.com>
2021-09-08 18:53:52 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/flaviobenyaakov" target="_blank" rel="noreferrer">Flavio Rodrigues</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/flavio-rodrigues-763711187/" target="_blank" rel="noreferrer">Flavio Rodrigues</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
