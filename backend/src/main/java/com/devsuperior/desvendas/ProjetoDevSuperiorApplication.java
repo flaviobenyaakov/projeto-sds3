66322dda648da6fc9eaf0fe3cef7f48d749c811b
flaviobenyaakov <flaviobenyaakov@gmail.com>
2021-09-08 16:20:26 -0300

    Projeto Pronto

---
+++ backend/src/main/java/com/devsuperior/desvendas/ProjetoDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDevSuperiorApplication.class, args);
+	}
+
+}
